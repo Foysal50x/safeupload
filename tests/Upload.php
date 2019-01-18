2386718b6c55450a5606a0bb249e9f3d3f513fcc
Faisal Ahmed <contact.faisalahmed@gmail.com>
2019-01-18 22:05:41 +0600

    testCase PathResolve

--- tests/Upload.php
+++ tests/Upload.php
@@ -16,10 +16,27 @@ class Upload extends PHPUnit\Framework\TestCase
     {
         parent::__construct($name, $data, $dataName);
         $this->upload = new \Safe\Upload();
+
+        $_FILES = array(
+            'file'    =>  array(
+                'name'      =>  'images.jpeg',
+                'tmp_name'  =>  __DIR__ . '/files/images.jpeg',
+                'type'      =>  'image/jpeg',
+                'size'      =>  6818,
+                'error'     =>  0
+            )
+        );
     }
 
     public function testSetUploadPath(){
+        $pathResolve = new \Safe\Resolver\Path('uploads/');
+        try{
+            $pathResolve->validate();
+        }catch (Exception $exception){
+            $this->assertNotEmpty($exception);
+        }
 
+        $this->assertEquals($pathResolve->getDestination(),'uploads/');
     }
 
 }
